5c22677221a62c400913d985733b3226912625c6
s10th24b <allen246@naver.com>
2021-03-20 00:29:44 +0900

    징검다리 건너기 added

---
+++ Kimhyoju/3_19/징검다리 건너기.kt
@@ -0,0 +1,21 @@
+package `3_19`
+
+fun main() {
+
+    class Solution {
+        fun solution(stones: IntArray, k: Int): Int {
+            var answer = 0
+
+
+            return answer
+        }
+    }
+    val sol = Solution()
+    var stones: IntArray
+    var k: Int
+
+    stones = intArrayOf(2, 4, 5, 3, 2, 1, 4, 2, 5, 1)
+    k = 3
+
+    println(sol.solution(stones,k))
+}
