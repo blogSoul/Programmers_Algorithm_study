68aa877b6215e25cd0f295b50fcbc96abebdabb1
cookyr <lawproclaim@gmail.com>
2021-04-27 23:51:25 +0900

    별찍기7

--- Cookyr/test.cpp
+++ Cookyr/test.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
-#include <string>
-#include <vector>
+
 
 using namespace std;
 
 
-void main(void)
+int main(void)
 {
+	int input = 0;
 
-	cout << "hello world" << endl;
+	cin >> input;
 
+	for (int i = 0; i<input; i++)
+	{
+		for (int j = 0; j < (input - i -1); j++)
+		{
+			cout << " ";
+		}
+
+		for (int k = 0; k < i; k++)
+		{
+			cout << "*";
+		}
+
+		cout << "*";
+
+		for (int k = 0; k < i; k++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+	for (int i = input-1; i > 0; i--)
+	{
+		for (int j = 0; j < (input - i); j++)
+		{
+			cout << " ";
+		}
+
+		for (int k = 1; k < i; k++)
+		{
+			cout << "*";
+		}
+
+		cout << "*";
+
+		for (int k = 1; k < i; k++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+	}
+
+
+	return 0;
 }
