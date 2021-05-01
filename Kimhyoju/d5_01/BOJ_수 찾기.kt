44f2f8df3de87f871ab819bc96da5d9833bf15b0
s10th24b <gywn6209@gmail.com>
2021-05-01 23:01:50 +0900

    BOJ_수 찾기 solved, BOJ_숫자카드2 failed and solved

---
+++ Kimhyoju/d5_01/BOJ_수 찾기.kt
@@ -0,0 +1,35 @@
+package d5_01
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+    val n = br.readLine().toInt()
+    val numbers = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val m = br.readLine().toInt()
+    val targets = br.readLine().split(" ").map { it.toInt() }
+    br.close()
+
+    for (target in targets) {
+        var left = 0
+        var right = n-1
+        var mid = 0
+        var found = 0
+        while (left <= right) {
+            mid = (left+right)/2
+            if (numbers[mid] > target) right = mid-1
+            else if (numbers[mid] < target) left = mid+1
+            else {
+                found = 1
+                break
+            }
+        }
+        println(found)
+        bw.write("$found\n")
+    }
+    bw.close()
+}
---
+++ Kimhyoju/d5_01/BOJ_숫자 카드 2.kt
@@ -0,0 +1,103 @@
+package d5_01
+
+// 일단 target을 찾고, 양옆으로 for문으로 찾는 방식을 택했는데, 이렇게 하니까 시간초과가 난다...
+// 그러면서 덤으로 Parametric Search도 찾아봤다.
+
+// 음... 설마 target을 찾고, answer+1 한다음, 그 target의 양옆으로 또 binary search 하는건가? DFS 로.
+
+// 하나씩 뻗어나가면 최악의 경우 똑같은 값이 10만 개 있고 그 값을 10만 개 찾게 되면 전체를 탐색하는 것과 다를 게 없어집니다.
+// 음... 그럼 lowerbound와 upperbound를 설정을 해야하는 건가..?
+
+// https://jackpot53.tistory.com/33
+// 여기서 upper bound와 lower bound를 제대로 설명해주고있다. 같은 값일때 자주 쓰이는 알고리즘.
+// 두 bound를 보아하니, lower bound는 같은 값일때 가장 먼저 나오는 값, upper bound는 같은 값일 때 가장 나중의 값.
+// 즉, Parametric Search와 같아보인다.
+
+// 이진탐색과 다른점은 크기가 9인  int test1[] = {1,2,2,3,3,3,4,6,7} 이 주어질때 이진탐색은 정확히 같은 값이 있는곳을 찾는거지만
+// lower bound는 주어진 값보다 같거나 큰 값이 처음으로 나오는걸 리턴해야 하는데 만약 lower_bound(9) 면 주어진 배열크기 만큼
+// 리턴되어야 하기 때문에 high = array.length -1 이 아니고 high = array.length로 지정 해야 한다.
+// 그리고 탐색한 값이  주어진 값보다 크거나 같으면 바로 리턴하지않고 같거나 처음으로 나오는 값을 찾기위해 범위를 더 좁히면서 찾아간다.
+// 크거나 같은경우 high = mid 로 지정해서 범위를 좀더 좁혀 나가면서 찾아가는 것이다.
+
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+lateinit var cards: List<Int>
+fun mainWithNaiveBinarySearch() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+    val n = br.readLine().toInt()
+    cards = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val m = br.readLine().toInt()
+    val targets = br.readLine().split(" ").map { it.toInt() }
+    for (target in targets) {
+        var left = 0
+        var right = n - 1
+        var mid = 0
+        var res = binarySearch(left, right, target)
+        bw.write("$res ")
+    }
+    bw.close()
+}
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.`out`))
+    val n = br.readLine().toInt()
+    cards = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val m = br.readLine().toInt()
+    val targets = br.readLine().split(" ").map { it.toInt() }
+    br.close()
+    for (target in targets) {
+        var left = 0
+        var right = n - 1
+        var mid = 0
+        var res = upperBound(target) - lowerBound(target)
+        bw.write("$res ")
+    }
+    bw.close()
+}
+
+fun lowerBound(target: Int): Int {
+    var low = 0
+    var high = cards.size // size -1 이 아닌, size
+    while (low < high) {
+        val mid = (low + high) / 2
+        if (target <= cards[mid]) high = mid
+        else low = mid + 1
+    }
+    return low
+}
+
+// lower bound와 마찬가지로 upper bound는 주어진 값보다  큰 값이 처음으로 나오는걸 리턴해야 하는데
+// 만약 upper_bound(9) 면 주어진 배열크기 만큼 리턴되어야 하기 때문에 high = array.length -1 이 아니고
+// high = array.length로 지정 해야 한다.
+// 그리고 탐색한 값이 주어진 값보다 크다면  바로 리턴하지 않고 최초로 큰 값이 있는 위치를  찾기 위해
+// high = mid 지정하여 범위를 더 좁히면서 찾아가면 된다.
+fun upperBound(target: Int): Int {
+    var low = 0
+    var high = cards.size // size -1 이 아닌, size
+    while (low < high) {
+        val mid = (low + high) / 2
+        if (target >= cards[mid]) low = mid + 1
+        else high = mid
+    }
+    return low
+}
+
+fun binarySearch(left: Int, right: Int, target: Int): Int {
+    if (left > right) return 0
+    var res = 0
+    var mid = (left + right) / 2
+    if (cards[mid] > target) res = binarySearch(left, mid - 1, target)
+    else if (cards[mid] < target) res = binarySearch(mid + 1, right, target)
+    else {
+        res++
+        res += binarySearch(left, mid - 1, target)
+        res += binarySearch(mid + 1, right, target)
+    }
+    return res
+}
