219e6401fcfb1b33c244188dc88526c02b4b2f3e
s10th24b <allen246@naver.com>
2021-05-15 23:24:34 +0900

    git reset

--- Kimhyoju/d05_14/BOJ_평범한_배낭.kt
+++ Kimhyoju/d05_14/BOJ_평범한_배낭.kt
@@ -8,33 +8,31 @@ import java.io.OutputStreamWriter
 // 대표적인 DP문제인 냅색문제라고 한다.
 // 점화식 찾기가 너무 어려운 것 같다..
 class BOJ_평범한_배낭 {
-    lateinit var dp: Array<MutableList<Pair<Int, Int>>>
+    lateinit var dp: Array<IntArray>
     fun main() {
         val br = BufferedReader(InputStreamReader(System.`in`))
         val bw = BufferedWriter(OutputStreamWriter(System.`out`))
         val (n, k) = br.readLine().split(" ").map { it.toInt() }
-        dp = Array(n) { mutableListOf<Pair<Int, Int>>() }
-        var answer = 0
+        val weights = mutableList<Int>()
+        val values = mutableList<Int>()
+        var MAX_WEIGHT = 0
+        var MAX_VALUE = 0
         for (i in 0 until n) {
             val (w, v) = br.readLine().split(" ").map { it.toInt() }
-            if (i > 0) {
-                for (j in 0..i) dp[i].add(dp[i - 1][j])
-                for ((weight, value) in dp[i - 1]) {
-                    val newWeight = weight + w
-                    if (newWeight > k) continue
-                    val newValue = value + v
-                    dp[i].add(Pair(newWeight, newValue))
-                    answer = maxOf(answer, newValue)
-                }
-            } else {
-                dp[i].add(Pair(0, 0))
-                dp[i].add(Pair(w, v))
-                if (w <= k) answer = v
-            }
-            println("dp: ${dp.toList()}")
+            weights.add(w)
+            values.add(v)
+            MAX_VALUE = maxOf(v, MAX_VALUE)
+            MAX_WEIGHT = maxOf(w, MAX_WEIGHT)
         }
-        println("final dp: ${dp.toList()}")
-
+        dp = Array(MAX_WEIGHT + 1) { IntArray(MAX_WEIGHT + 1) { 0 } } // dp[i] : 무게가 i 일때 최대 가치
+        for (i in weights.indices) {
+            val w = weights[i]
+            val v = values[i]
+            for (j in w until weights.size) {
+                dp[j] = maxOf(dp[j])
+            }
+        }
+        var answer = 0
         bw.write("$answer")
         br.close()
         bw.close()
